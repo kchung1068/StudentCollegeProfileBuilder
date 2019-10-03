39f3018a54bcefdff6827517390c2de46b2bd67d
Kyle Chung <kchung1068@stu.d214.org>
2019-10-03 13:58:06 -0500

    Initial Commit

---
+++ StudentCollegeProfileBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentCollegeProfileBuilder
+//
+//  Created by Kyle C on 10/3/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
